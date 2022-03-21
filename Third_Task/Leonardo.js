5df3120a82fb4fbd891fad7ec407e72c9f4f32f4
pedro.ferreira@findmore.eu <pedro.ferreira@findmore.eu>
2022-03-21 17:48:35 +0000

    added third task

---
+++ Third_Task/Leonardo.js
@@ -0,0 +1,12 @@
+//Bonus Time!
+//Bonus 1:
+//Go to lorem ipsum generator and:
+
+//Generate 3 paragraphs. Store the text in a variable type of string.
+
+//Make your program count the number of words in the string.
+
+//Make your program count the number of times the Latin word et appears.
+
+//Bonus 2:
+//Create a new variable phraseToCheck and have it contain some string value. Write a code that will check if the value we assigned to this variable is a Palindrome.
